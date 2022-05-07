b6bed0b41207355fa31265c9ee8a8931c511f317
KARUNADA KING <97476963+MR-KANNADIGA@users.noreply.github.com>
2022-05-08 02:51:37 +0530

    Update chatbot.py

--- FallenRobot/modules/chatbot.py
+++ FallenRobot/modules/chatbot.py
@@ -40,7 +40,7 @@ def kukirm(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Agora Robot Chatbot Disabled By {}.".format(mention_html(user.id, user.first_name)),
+                "BTS PURPLE Robot Chatbot Disabled By {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
@@ -66,7 +66,7 @@ def kukiadd(update: Update, context: CallbackContext) -> str:
             )
         else:
             update.effective_message.edit_text(
-                "Agora Robot Chatbot Enabled By {}.".format(mention_html(user.id, user.first_name)),
+                "BTS PURPLE Robot Chatbot Enabled By {}.".format(mention_html(user.id, user.first_name)),
                 parse_mode=ParseMode.HTML,
             )
 
